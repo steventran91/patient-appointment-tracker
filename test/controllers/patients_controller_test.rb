6987bcd5fc21091ff96cf006c8ede7d17fea2989
Steven Tran <sdtran91@gmail.com>
2026-06-09 17:04:30 -0700

    create tests for patient and provider controllers

--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class PatientsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get all patients" do 
+    get "/patients", headers: { 'Authorization' => "Bearer #{providers(:peter).token}" }
+    assert_response :success
+  end
+
+  test "create patient successfully" do 
+    post "/patients", headers: { 'Authorization' => "Bearer #{providers(:peter).token}"},
+    params: {patient: {first_name: "Naruto", last_name: "Uzumaki", email: "7hokage@gmail.com", date_of_birth: '01/01/2000'}},
+    as: :json
+    assert_response :created
+  end
+
+  test "return error with invalid data" do
+    post "/patients", headers: { 'Authorization' => "Bearer #{providers(:peter).token}"},
+    params: {patient: {last_name: "Uzumaki", email: "7hokage@gmail.com", date_of_birth: '01/01/2000'}},
+    as: :json
+    assert_response :unprocessable_entity
+  end
+
+  test "get patient by id" do
+    patient = patients(:john)
+    get "/patients/#{patient.id}", headers: { 'Authorization' => "Bearer #{providers(:peter).token}"}
+    assert_response :success
+  end
+
+  test "update patient info" do 
+    patient = patients(:john)
+    patch "/patients/#{patient.id}", headers: { 'Authorization' => "Bearer #{providers(:peter).token}"},
+    params: {patient: {first_name: "Boruto", last_name: "Uzumaki", email: "7hokage@gmail.com", date_of_birth: '01/01/2000'}},
+    as: :json
+    assert_response :success
+  end
 end
--- test/controllers/providers_controller_test.rb
+++ test/controllers/providers_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class ProvidersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    test "get all providers" do 
+      get "/providers", headers: {'Authorization' => "Bearer #{providers(:peter).token}"}
+      assert_response :success
+    end
+
+    test "create a provider successfully" do 
+      post "/providers", headers: {'Authorization' => "Bearer #{providers(:peter).token}"},
+      params: {provider: {first_name: "Tony Tony", last_name: "Chopper", specialty: "Medicine", email: "tonytonychopper@gmail.com"}},
+      as: :json
+      assert_response :created
+    end
+
+    test "return error with invalid data" do
+      post "/providers", headers: {'Authorization' => "Bearer #{providers(:peter).token}"},
+      params: {provider: {first_name: "Tony Tony", last_name: "Chopper", email: "tonytonychopper@gmail.com"}},
+      as: :json
+      assert_response :unprocessable_entity
+    end
+
+    test "get provider by id" do 
+      provider = providers(:peter)
+      get "/providers/#{provider.id}", headers: {'Authorization' => "Bearer #{providers(:peter).token}"}
+      assert_response :success
+    end
+
+    test "update a provider" do 
+      provider = providers(:peter)
+      patch "/providers/#{provider.id}", headers: {'Authorization' => "Bearer #{providers(:peter).token}"},
+      params: {provider: {first_name: "Peter", last_name: "Porker", specialty: "Spider-Man", email: "peterparker@gmail.com"}},
+      as: :json
+      assert_response :success
+    end
+
+  test "delete a provider" do
+    provider = Provider.create(first_name: "Tony Tony", last_name: "Chopper", specialty: "Medicine", email: "chopper@gmail.com")
+    delete "/providers/#{provider.id}", headers: { 'Authorization' => "Bearer #{providers(:peter).token}" }
+    assert_response :no_content
+  end
 end
